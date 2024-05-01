5f9d521a17fe0fab85dae55de1d9fa4866b6f76d
CGandGameEngineLearner <1353321741@qq.com>
2024-05-01 23:57:20 +0800

    Feature: lab4-3

---
+++ regular_and_requests/右键运行此PowerShell脚本自动初始化开发环境.ps1
@@ -0,0 +1,29 @@
+# 定义 Python 安装程序的下载链接和文件名 通过华为云下载python 3.8.6安装包
+$installerUrl = "https://mirrors.huaweicloud.com/python/3.8.6/python-3.8.6-amd64.exe"
+$installerFile = "python-3.8.6-amd64.exe"
+
+# 下载 Python 安装程序
+Write-Host "正在下载 Python 安装程序..."
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerFile
+
+# 安装 Python
+Write-Host "正在安装 Python，请稍候..."
+Start-Process -FilePath $installerFile -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
+
+# 删除安装程序
+Write-Host "Python 安装完成！删除安装程序..."
+Remove-Item $installerFile
+
+Write-Host "Python 3.8.6 已安装成功"
+
+# 创建虚拟环境
+python -m venv venv
+
+# 激活虚拟环境
+.\venv\Scripts\Activate.ps1
+
+# 通过镜像源pip安装项目依赖库
+pip install -r requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simple/
+
+# 通过jupyter打开
+jupyter notebook ./src/main.ipynb
