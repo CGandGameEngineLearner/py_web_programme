a666749c697c5d6d0757070dbe286f415b01fd65
CGandGameEngineLearner <1353321741@qq.com>
2024-05-05 22:51:05 +0800

    Fixed: ps1

--- beautiful_soup/右键运行此PowerShell脚本自动初始化开发环境.ps1
+++ beautiful_soup/右键运行此PowerShell脚本自动初始化开发环境.ps1
@@ -2,13 +2,16 @@
 $installerUrl = "https://mirrors.huaweicloud.com/python/3.8.6/python-3.8.6-amd64.exe"
 $installerFile = "python-3.8.6-amd64.exe"
 
-# 获取所有已安装的Python版本
-$pythonVersions = Get-ChildItem -Path "HKLM:\SOFTWARE\Python\PythonCore" | ForEach-Object { $_.PSChildName }
+# 定义安装路径
+$userInstallPath = "$env:LOCALAPPDATA\Programs\Python\Python38"
+
+# 获取当前Windows用户所有已安装的Python版本
+$pythonVersions = Get-ChildItem -Path "HKCU:\SOFTWARE\Python\PythonCore" | ForEach-Object { $_.PSChildName }
 
 # 检查是否有3.8.6版本
 $python386Installed = $false
 foreach ($version in $pythonVersions) {
-    Write-Host $version
+    Write-Host "检测到本机已有 Python "+$version
     if ($version -eq "3.8.6") {
         $python386Installed = $true
         break
@@ -31,7 +34,20 @@ if ($python386Installed -eq $false)
 
     # 安装 Python
     Write-Host "正在安装 Python，请稍候..."
-    Start-Process -FilePath $installerFile -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
+    # 运行 Python 3.8.6 安装程序
+    Start-Process -FilePath $installerFileName -ArgumentList "/quiet", "InstallAllUsers=0", "PrependPath=1", "DefaultJustForMeTargetDir=`"$userInstallPath`"" -Wait
+
+    # 将 Python 3.8.6 添加到系统环境变量中
+    $env:Path += ";$userInstallPath"
+
+    # 将 Python 3.8.6 添加到注册表
+    $pythonPath = Join-Path $userInstallPath "python.exe"
+    $pythonVersion = "3.8.6"
+    $pythonKey = "HKCU:\Software\Python\PythonCore\$pythonVersion"
+    New-Item -Path $pythonKey
+    New-ItemProperty -Path $pythonKey -Name "(Default)" -Value $pythonVersion
+    New-ItemProperty -Path $pythonKey -Name "InstallPath" -Value $userInstallPath
+    New-ItemProperty -Path $pythonKey -Name "ExecutablePath" -Value $pythonPath
 
     # 删除安装程序
     Write-Host "Python 安装完成！删除安装程序..."
